994b7172a53125f6afec76b79f4a9bd6c5f37691
BrandMaker.now <l6598668@gmail.com>
2026-02-14 16:46:11 +0900

    Update news-data.js

--- news-data.js
+++ news-data.js
@@ -1,6 +1,7 @@
 window.newsData = [
-{ date: "2026-02-04", category: "メンテナンス", title: "【緊急】システムメンテナンスについて", folder: "2026-02-04-【緊急】システムメンテナンスについて", pinned: true, isFinished: true },
-{ date: "2026-02-09", category: "重要", title: "WorldNowサービス終了のお知らせ", folder: "2026-02-09-WorldNow サービス終了のお知らせ", pinned: false, isFinished: true }
+{ date: "2026-02-03", category: "メンテナンス", title: "システムメンテナンスについて", folder: "2026-02-03-システムメンテナンスについて", pinned: false, isFinished: false },
+{ date: "2026-02-04", category: "メンテナンス", title: "【緊急】システムメンテナンスについて", folder: "2026-02-04-【緊急】システムメンテナンスについて", pinned: true, isFinished: false },
+{ date: "2026-02-09", category: "重要", title: "WorldNowサービス終了のお知らせ", folder: "2026-02-09-WorldNow サービス終了のお知らせ", pinned: false, isFinished: false }
 ];
 
 function displayNews(filter = 'すべて', keyword = '') {
